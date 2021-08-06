a3b36867f07f14cd28699839e2c7aca72d3e4574
asonnleitner <andreassonnleitner@gmail.com>
2021-08-06 18:08:15 +0200

    init

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import * as express from 'express'
+
+const app = express.default()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+})
+
+const PORT = process.env.PORT || 3000
+
+console.log('Deda')
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server is running on http://localhost:${PORT}`)
+})
